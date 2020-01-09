e0eefc3c90da4e5d992e066c961c776fee91fc7d
Paul <paul@epcc-gpu-training>
2020-01-09 14:10:13 +0000

    Doing exercises 1, 2, 3 of cuda-intro

--- exercises/cuda-intro/src_c/intro.cu
+++ exercises/cuda-intro/src_c/intro.cu
@@ -14,7 +14,7 @@
 void checkCUDAError(const char*);
 
 /* The number of integer elements in the array */
-#define ARRAY_SIZE 256
+#define ARRAY_SIZE 513
 
 /*
  * The number of CUDA blocks and threads per block to use.
@@ -23,7 +23,7 @@ void checkCUDAError(const char*);
  * THREADS_PER_BLOCK should be the array size
  */
 
-#define NUM_BLOCKS  1
+#define NUM_BLOCKS 2
 #define THREADS_PER_BLOCK 256
 
 /* The actual array negation kernel (basic single block version) */
@@ -31,7 +31,9 @@ void checkCUDAError(const char*);
 __global__ void negate(int * d_a) {
 
   /* Part 2B: negate an element of d_a */
+  int idx = threadIdx.x;
 
+  d_a[idx] *= -1;
 }
 
 /* Multi-block version of kernel for part 2C */
@@ -39,7 +41,12 @@ __global__ void negate(int * d_a) {
 __global__ void negate_multiblock(int *d_a) {
 
   /* Part 2C: negate an element of d_a, using multiple blocks this time */
-
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  
+  if (idx < ARRAY_SIZE)
+  {
+    d_a[idx] = -d_a[idx];
+  }
 }
 
 /* Main routine */
@@ -51,6 +58,8 @@ int main(int argc, char *argv[]) {
 
   int i;
   size_t sz = ARRAY_SIZE * sizeof(int);
+
+  cudaError_t err;
 
   /* Print device details */
 
@@ -72,7 +81,7 @@ int main(int argc, char *argv[]) {
    * allocate memory on device
    */
   /* Part 1A: allocate device memory */
-
+  err = cudaMalloc(&d_a, sz);
 
   /* initialise host arrays */
   for (i = 0; i < ARRAY_SIZE; i++) {
@@ -82,14 +91,34 @@ int main(int argc, char *argv[]) {
 
   /* copy input array from host to GPU */
   /* Part 1B: copy host array h_a to device array d_a */
-
+  err = cudaMemcpy(d_a, h_a, sz, cudaMemcpyHostToDevice);
 
   /* run the kernel on the GPU */
   /* Part 2A: configure and launch kernel (un-comment and complete) */
-  /* dim3 blocksPerGrid( ); */
-  /* dim3 threadsPerBlock( ); */
-  /* negate<<< , >>>( ); */
+  int num_blocks, rem;
+  num_blocks = ARRAY_SIZE / THREADS_PER_BLOCK;
+  rem = ARRAY_SIZE - num_blocks * THREADS_PER_BLOCK;
+  if (rem > 0)
+  {
+    num_blocks++;
+  }
+  else if ((rem < 0) || !num_blocks)
+  {
+    printf("ERROR: THREADS_PER_BLOCK is too big!");
+  }
 
+  dim3 blocksPerGrid(num_blocks, 1, 1);
+  dim3 threadsPerBlock(THREADS_PER_BLOCK, 1, 1);
+  if (num_blocks == 1)
+  {
+    negate<<< blocksPerGrid, threadsPerBlock >>>(d_a);
+  }
+  else
+  {
+    negate_multiblock<<< blocksPerGrid, threadsPerBlock >>>(d_a);
+  }
+  printf("Running with %i blocks x %i threads/block for %i elements\n",
+         num_blocks, THREADS_PER_BLOCK, ARRAY_SIZE); 
 
   /* wait for all threads to complete and check for errors */
 
@@ -98,6 +127,7 @@ int main(int argc, char *argv[]) {
 
   /* copy the result array back to the host */
   /* Part 1C: copy device array d_a to host array h_out */
+  err = cudaMemcpy(h_out, d_a, sz, cudaMemcpyDeviceToHost);
 
   checkCUDAError("memcpy");
 
@@ -110,6 +140,7 @@ int main(int argc, char *argv[]) {
 
   /* free device buffer */
   /* Part 1D: free d_a */
+  err = cudaFree(d_a);
 
   /* free host buffers */
   free(h_a);
